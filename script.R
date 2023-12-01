a9110c08d4be84102a2353b4ba6ed327d600bcb3
aeiouxx <st55776@upce.cz>
2023-12-01 09:03:56 +0100

    contingency table

--- script.R
+++ script.R
@@ -1,59 +1,58 @@
-####1. DATA IMPORT ####
+# install.packages("dplyr")
+library(dplyr)
 data = read.csv('all_stocks_5yr.csv');
-str(data);
-summary(data);
-####2. DATA CLEANUP    ####
-data$date = as.Date(data$date, format = "%Y-%m-%d"); # convert date to date format
+data <- data[data$Name %in% c("JPM", "BAC"), ]
+data$date = as.Date(data$date, format = "%Y-%m-%d");
 colSums(is.na(data)); # check for missing entries 
-#   date   open   high    low  close volume   Name 
-#      0     11      8      8      0      0      0 
-# remove rows with missing values (could also impute but not necessary because we have a lot of data and the missing data seems to be random)
-data <- na.omit(data);
-###3. ADDITIONAL DATA MANIPULATION ####
-data$daily_return <- with(data, (close - open)/open); # add daily return column
-data$log_return <- with(data, log(close/open)); # add log return column (tends to be more normally distributed thad raw prices, also time additive)
-# install.packages("zoo");
-library(zoo);
-# Group data by stock name, sort by date, calculate moving average of closing price per 20 days (20-21 trading days in a month), add as a column and remove grouping
-data <- data %>% group_by(Name) %>% arrange(date) %>% mutate(ma = rollmean(close, 20, fill = NA, align = "right")) %>% ungroup();
-# visualization example:
-apple_data <- data %>% filter(Name == "AAPL");
-ggplot(apple_data, aes(x = date)) +
-  geom_line(aes(y = close), color = "blue") +
-  geom_line(aes(y = ma), color = "red") +
-  labs(title = "Apple Stock Price and 20-Day Moving Average Over Time",
-       x = "Date",
-       y = "Price") +
-  theme_minimal()
-
-# calculate correlation matrices 
-# install.packages("tidyr");
-library(tidyr);
-company_correlations <- raw_data %>%
+data <- na.omit(data); # yeet
+data <- data %>%
   group_by(Name) %>%
-  nest() %>%
-  mutate(correlation_matrix = map(data, ~ cor(select(.x, open, high, low, close, volume, log_returns), 
-                                             use = "complete.obs")))
-apple_correlation_matrix <- company_correlations %>%
-  filter(Name == "AAPL") %>%
-  pull(correlation_matrix) %>%
-  .[[1]]
-print(apple_correlation_matrix)
-
-# calculate covariance matrices
-company_covariances <- data %>%
+  arrange(date) %>%
+  mutate(change = (close - lag(close))/lag(close) * 100);
+# CHI-SQUARE TEST
+# 1. KATEGORIE VOLUME
+categorize_by_volume <- function(volume) {
+  thirdQuantile <- quantile(volume, 0.33)
+  twoThirdQuantile <- quantile(volume, 0.66)
+  categorize <- function(volume) {
+    if (volume < thirdQuantile) {
+      return("LOW")
+    } else if (volume < twoThirdQuantile) {
+      return("MEDIUM")
+    } else {
+      return("HIGH")
+    }
+  }
+  sapply(volume, categorize)
+}
+# 2. KATEGORIE RETURN
+categorize_change <- function(change, low_threshold, moderate_threshold, high_threshold) {
+  abs_change <- abs(change)
+  if (is.na(change)) {
+    return(NA)
+  } else if (abs_change <= low_threshold) {
+    return("NONE")
+  } else if (abs_change <= moderate_threshold) {
+    return("LOW")
+  } else if (abs_change <= high_threshold) {
+    return("MED")
+  } else {
+    return("HIG")
+  }
+}
+# 3.
+quantiles <- quantile(abs(data$change), probs = c(0.10, 0.50, 0.90), na.rm = TRUE)
+low_threshold <- quantiles[1] # 10th percentile
+medium_threshold <- quantiles[2] # 50th percentile
+high_threshold <- quantiles[3] # 90th percentile
+data <- data %>% 
   group_by(Name) %>%
-  nest() %>%
-  mutate(covariance_matrix = map(data, ~ cov(select(.x, open, high, low, close, volume, log_returns), 
-                                            use = "complete.obs")))
-apple_covariance_matrix <- company_covariances %>%
-  filter(Name == "AAPL") %>%
-  pull(covariance_matrix) %>%
-  .[[1]]
-print(apple_covariance_matrix)
-# descriptive statistics
-# data visualization
-# regression analysis
-# time series
-# hypothesis testing
-# ANOVA
+  mutate(volume_category = categorize_by_volume(volume),
+         change_category = sapply(change, categorize_change, low_threshold, medium_threshold, high_threshold))
+bac_data <- data[data$Name == "BAC",];
+jpm_data <- data[data$Name == "JPM",];
+
+bac_contin_table <- table(bac_data$volume_category, bac_data$change_category);
+jpm_contin_table <- table(jpm_data$volume_category, jpm_data$change_category);
+chisq.test(bac_contin_table);
+chisq.test(jpm_contin_table);
